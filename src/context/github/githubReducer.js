9469253bf6950126d9e3fd65f2a83c5790989309
jon91@utexas.edu <jon91@utexas.edu>
2019-06-28 14:27:58 -0700

    Moving repos state to context

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -16,16 +16,22 @@ export default (state, action) => {
       };
     case GET_USER:
       return {
-      ...state,
-      user: action.payload,
-      loading: false
-    }
+        ...state,
+        user: action.payload,
+        loading: false
+      };
     case CLEAR_USERS:
       return {
         ...state,
         users: [],
         loading: false
-      }
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: action.payload,
+        loading: false
+      };
     case SET_LOADING:
       return {
         ...state,
